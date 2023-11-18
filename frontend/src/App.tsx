274c03611e3384c626d5ea64c20d9e292e566c2c
Crypto-kiki <ejo2123@naver.com>
2023-11-18 13:08:54 +0900

    sign-in sign-up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import { FC } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/main";
+import SignIn from "./pages/sign-in";
+import SignUp from "./pages/sign-up";
 
 const App: FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/log-in" element={<Main />} />
-        <Route path="/sign-up" element={<Main />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/create" element={<Main />} />
         <Route path="/:postId" element={<Main />} />
       </Routes>
--- frontend/src/pages/main.tsx
+++ frontend/src/pages/main.tsx
@@ -1,7 +1,26 @@
 import { FC } from "react";
 
 const Main: FC = () => {
-  return <div>Main</div>;
+  return (
+    <>
+      <header>헤더</header>
+      <main className="max-w-screen-md mx-auto">
+        <h1 className="mt-20 text-center font-bold text-2xl">h662's 게시판</h1>
+        <ul className="mt-10 h-[440px]">
+          <li className="flex justify-between border-b-2 font-semibold">
+            <span className="w-2/12 p-2 text-center">아이디</span>
+            <span className="w-6/12 p-2 text-center">제목</span>
+            <span className="w-2/12 p-2 text-center">사용자</span>
+            <span className="w-2/12 p-2 text-center">작성일</span>
+          </li>
+          <li>포스트 카드</li>
+        </ul>
+        <ul className="flex text-lg justify-center">
+          <li>페이지</li>
+        </ul>
+      </main>
+    </>
+  );
 };
 
 export default Main;
