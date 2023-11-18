1a157494d246c41198bebf2d45b75723b7ac0fca
Crypto-kiki <ejo2123@naver.com>
2023-11-18 10:11:10 +0900

    prisma set

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,18 @@
-import express, { Express } from "express";
+import express, { Express, urlencoded } from "express";
+import cors from "cors";
 
 const app: Express = express();
 const port: Number = +process.env.PORT! | 3010;
+
+// midleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.FRONT_URL,
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello Express TS!");
